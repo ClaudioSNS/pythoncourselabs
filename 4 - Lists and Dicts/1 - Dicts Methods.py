79585d27eaf9dc84f2a960a2662d776c655b48e5
ClaudioSNS <sansone.claudio@gmail.com>
2022-09-22 15:52:52 +0200

    TEST primi esercizi

--- 4 - Lists and Dicts/1 - Dicts Methods.py
+++ 4 - Lists and Dicts/1 - Dicts Methods.py
@@ -12,43 +12,43 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 See the License for the specific language governing permissions and limitations under the License
 '''
 
-d = {
-    "category": "animal",
-    "name": "felix"
-}
-print(f"d: {d}")
-print(f"d['name']: {d['name']}")
+# d = {
+#     "category": "animal",
+#     "name": "felix"
+# }
+# print(f"d: {d}")
+# print(f"d['name']: {d['name']}")
+#
+# dd = {
+#     "typology": {
+#         "category": "animal",
+#         "subcategory": "cat"
+#     },
+#     "name": "felix"
+# }
+# print(f"dd: {dd}")
+# print(f"dd['typology']: {dd['typology']}")
+# print(f"dd['typology']['subcategory']: {dd['typology']['subcategory']}")
 
-dd = {
-    "typology": {
-        "category": "animal",
-        "subcategory": "cat"
-    },
-    "name": "cat"
-}
-print(f"dd: {dd}")
-print(f"dd['typology']: {dd['typology']}")
-print(f"dd['typology']['subcategory']: {dd['typology']['subcategory']}")
-
-print(f"Is 'felix' in d? {'felix' in d}")
-print(f"Is 'felix' in d? {'felix' in d.values()}")
-print(f"d.values() {d.values()}")
-print(f"Is 'name' in d? {'name' in d}")
-print(f"Is 'name' in d? {'name' in d.keys()}")
-print(f"d.keys() {d.keys()}")
-
-d['colour'] = 'white'
-print(f"d: {d}")
-
-colour = d.pop('colour')
-print(f"colour: {colour}")
-print(f"d: {d}")
-
-d.clear()
-print(f"d: {d}")
+# print(f"Is 'felix' in d? {'felix' in d}") # d.keys()
+# print(f"Is 'felix' in d? {'felix' in d.values()}")
+# print(f"d.values() {d.values()}")
+# print(f"Is 'name' in d? {'name' in d}")
+# print(f"Is 'name' in d? {'name' in d.keys()}")
+# print(f"d.keys() {d.keys()}")
+#
+# d['colour'] = 'white'
+# print(f"d: {d}")
+#
+# colour = d.pop('colour')
+# print(f"colour: {colour}")
+# print(f"d: {d}")
+#
+# d.clear()
+# print(f"d: {d}")
 
 # ToDo: Uncomment the following piece of code, then print public values only and, finally, print email value.
-'''
+
 x = {
     "id": "10024",
     "personal info": {
@@ -70,4 +70,40 @@ x = {
         }
     }
 }
-'''
+
+# print(f"{x['personal info']['public']}")
+
+def esplora(d):
+    assert(isinstance(d, dict))
+    for k in d:
+        # print(k)
+        # print(d[k])
+        if isinstance(d[k], dict):
+            esplora(d[k])
+        else:
+            print(f"Key: {k} - Value: {d[k]}")
+
+# esplora(x)
+
+def esplora2(d):
+    assert(isinstance(d, dict))
+    for k, v in d.items():
+        if isinstance(v, dict):
+            esplora2(v)
+        else:
+            print(f"Key: {k} - Value: {v}")
+
+# esplora2(x)
+
+def esplora3(d):
+    assert(isinstance(d, dict))
+    for k, v in d.items():
+        if isinstance(v, dict):
+            if k == "public":
+                print(f"Key: {k} - Value: {v}")
+            esplora3(v)
+        else:
+            if k == "public" or k == "email":
+                print(f"Key: {k} - Value: {v}")
+
+esplora3(x)
